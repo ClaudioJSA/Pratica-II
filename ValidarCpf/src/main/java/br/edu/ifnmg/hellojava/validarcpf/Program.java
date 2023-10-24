c1d747947f90f4f7405ad725734a8ea977f32949
Claudio Juliano Santos Alcantara <54650277+ClaudioJSA@users.noreply.github.com>
2023-10-23 23:55:07 -0300

    Update Program.java

--- ValidarCpf/src/main/java/br/edu/ifnmg/hellojava/validarcpf/Program.java
+++ ValidarCpf/src/main/java/br/edu/ifnmg/hellojava/validarcpf/Program.java
@@ -12,6 +12,28 @@ package br.edu.ifnmg.hellojava.validarcpf;
 public class Program {
     public static void main(String[] args) {
         
+        System.out.println("CPF: 14230703058");
+        if(Util.validarCpf(14230703058L))
+            System.out.println("Resposta: Válido.\n");
+        else
+            System.out.println("Resposta: Inválido.\n");
         
+        System.out.println("CPF: 142.307.030-58");
+        if(Util.validarCpf("142.307.030-58"))
+            System.out.println("Resposta: Válido.\n");
+        else
+            System.out.println("Resposta: Inválido.\n");
+        
+        System.out.println("CPF: 230703058");
+        if(Util.validarCpf(230703058L))
+            System.out.println("Resposta: Válido.\n");
+        else
+            System.out.println("Resposta: Inválido.\n");
+        
+        System.out.println("CPF: 002.307.030-58");
+        if(Util.validarCpf("002.307.030-58"))
+            System.out.println("Resposta: Válido.\n");
+        else
+            System.out.println("Resposta: Inválido.\n");
     }
 }
